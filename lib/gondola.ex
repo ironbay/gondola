4f0a9d594ecf742482fef8dedc472ec86bf298c2
Dax Ravi <d@ironbay.co>
2019-02-14 09:08:13 -0500

    Caching

--- lib/gondola.ex
+++ lib/gondola.ex
@@ -3,9 +3,30 @@ alias Gondola.Config
 
 defmodule Gondola do
   def for_scope(scope) do
-    Token.from_method(scope, Config.method())
+    case cache_get(scope) do
+      nil ->
+        {:ok, token} = Token.from_method(scope, Config.method())
+        :ok = cache_put(scope, token.expires_in + :os.system_time(:second) - 30, token)
+        {:ok, token}
+
+      token ->
+        IO.puts("From cache")
+        {:ok, token}
+    end
   end
 
-  def from_cache(scope) do
+  def cache_get(scope) do
+    now = :os.system_time(:second)
+
+    :gondola
+    |> Application.get_env(scope)
+    |> case do
+      {expires, token} when expires > now -> token
+      _ -> nil
+    end
+  end
+
+  def cache_put(scope, expires, token) do
+    Application.put_env(:gondola, scope, {expires, token})
   end
 end
